201ba6e1074b085ed233317d3f00542075ff6b52
Ben <52816705+benmorant@users.noreply.github.com>
2020-09-16 19:49:32 +0200

    + others files

---
+++ src/app/multicriteria-search/multicriteria-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MulticriteriaSearchComponent } from './multicriteria-search.component';
+
+describe('MulticriteriaSearchComponent', () => {
+  let component: MulticriteriaSearchComponent;
+  let fixture: ComponentFixture<MulticriteriaSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MulticriteriaSearchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MulticriteriaSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
