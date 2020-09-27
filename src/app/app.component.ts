6bd54e37a6e0dbb8cc2dd7a404e992a320f28b09
akatz <akatz@henix.fr>
2020-09-28 01:51:13 +0200

    New filters placement.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,4 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'My Sweet Paris';
-
-  stickToTop(): void {
-    const navBar = document.getElementById('navBar');
-    const stickyNavBar = navBar.offsetTop;
-    const navBarStandIn = document.getElementById('navBarStandIn');
-    if (window.pageYOffset > stickyNavBar) {
-      navBar.classList.add('he-stick-to-top');
-      navBarStandIn.classList.add('he-stand-in');
-      navBarStandIn.classList.remove('he-stand-out');
-    } else {
-      navBar.classList.remove('he-stick-to-top');
-      navBarStandIn.classList.add('he-stand-out');
-      navBarStandIn.classList.remove('he-stand-in');
-    }
-  }
 }
